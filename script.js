707fbcd8cb6729a9e194e924b03f8acd38f9a302
Andres Rosales <andresrosalespinzon@gmail.com>
2021-05-21 20:08:18 +0200

    packing all tests using describe

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const googleData = ['cats.com', 'dogs.com', 'jobs.com', 'pictures.com'];
 
-const googleSearch = (searchInput) => {
-  const matches = googleData.filter((website) => {
+const googleSearch = (searchInput, db) => {
+  const matches = db.filter((website) => {
     return website.includes(searchInput);
   });
   return matches.length > 3 ? matches.slice(0, 3) : matches;
 };
 
-console.log(googleSearch('s'));
+console.log(googleSearch('s', googleData));
 
 module.exports = googleSearch;
--- script.test.js
+++ script.test.js
@@ -1,5 +1,25 @@
 const googleSearch = require('./script');
 
-it('this is a test', () => {
-  console.log(googleSearch);
+const dummyDB = ['disney.com', 'hbo.com', 'amazon.com', 'fmovies.com'];
+describe('googleSearch', () => {
+  it('this is a test', () => {
+    expect('hello').toBe('hello');
+  });
+
+  it('is searching google', () => {
+    expect(googleSearch('elsdjh', dummyDB)).toEqual([]);
+    expect(googleSearch('hbo', dummyDB)).toEqual(['hbo.com']);
+  });
+
+  it('work with undef', () => {
+    expect(googleSearch(undefined, dummyDB)).toEqual([]);
+  });
+
+  it('work with null input', () => {
+    expect(googleSearch(null, dummyDB)).toEqual([]);
+  });
+
+  it('does not return more than 3 matches', () => {
+    expect(googleSearch('.com', dummyDB).length).toEqual(3);
+  });
 });
