98fc6bc2b07e3179d8a301a05b9528729a8e267b
Andres Rosales <andresrosalespinzon@gmail.com>
2021-05-22 17:18:41 +0200

    mocking fetch

--- Async-Test/script.test.js
+++ Async-Test/script.test.js
@@ -15,3 +15,23 @@ it('calls swapi with promise', () => {
     expect(data.results.length).toBeGreaterThan(5);
   });
 });
+
+it('getPeople returns count and results', () => {
+  const mockFetch = jest.fn().mockReturnValue(
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          count: 87,
+          results: [0, 1, 2, 3, 4, 5],
+        }),
+    })
+  );
+
+  expect.assertions(4);
+  return swapi.getPeoplePromise(mockFetch).then((data) => {
+    expect(mockFetch.mock.calls.length).toBe(1);
+    expect(mockFetch).toBeCalledWith('https://swapi.dev/api/people');
+    expect(data.count).toEqual(82);
+    expect(data.results.length).toBeGreaterThan(6);
+  });
+});
